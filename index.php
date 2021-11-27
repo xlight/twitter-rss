ec8fc703e380bea0109526ec164f1010a88adead
CodePoet80 <jonathan.m.wise@gmail.com>
2021-11-27 10:56:04 -0500

    start adding access control

--- index.php
+++ index.php
@@ -5,62 +5,101 @@ $bearer_token = $config['bearer_token'];
 if (!isset($config) || !isset($config['bearer_token']) || $config['bearer_token'] == "" || $config['bearer_token'] == "<YOURBEARERTOKENFROMTWITTER>") {
 	die ("No valid Twitter API v2 bearer token specified in config");
 }
+
+//Figure out who the user is and what they want
+//TODO: test more cases
 if ($_SERVER['QUERY_STRING'] != "") {
-	$uid = $_SERVER['QUERY_STRING'];
-	if (isset($config['require_encoding']) && $config['require_encoding'] == true)
-		$uid = base64_decode($uid);
-	if (!is_numeric($uid)) {
-		unset($uid);
+	$clientid = "anonymous";
+	if (isset($_GET['twitterid'])) {
+		$twitterid = $_GET['twitterid'];
+		if (isset($_GET['clientid'])) {
+			$clientid = $_GET['clientid'];
+		}
+	} else {
+		$twitterid = $_SERVER['QUERY_STRING'];
+	}
+	if (isset($config['require_encoding']) && $config['require_encoding'] == true) {
+		$twitterid = base64_decode($twitterid);
+		if ($clientid != "anonymous") {
+			$clientid = base64_decode($clientid);
+		}
+	}
+	if (!is_numeric($twitterid)) {
+		unset($twitterid);
+		die ("No valid Twitter ID specified. Valid Twitter IDs are numeric, and may need to be encoded. You can get the numeric ID from a username with sites like https://tweeterid.com/");
 	}
 }
 
-if (isset($uid)) {
-	$userdata = get_twitter_userdata($uid, $bearer_token);
-	$user_o = json_decode($userdata);
-
-	$tweetURL = "https://twitter.com/" . $user_o->data->username;
-	output_rss_header($user_o->data->name . " Tweets", $tweetURL, "@" . $user_o->data->username, $user_o->data->profile_image_url);
-	$tweetURL = $tweetURL . "/status/";
-	
-	$tweetdata = get_twitter_tweetsforuser($uid, $bearer_token);
-	$tweets_o = json_decode($tweetdata);
-	$tweets = $tweets_o->data;
-	if (isset($tweets_o->includes)) {
-		$includes = $tweets_o->includes;
-	}
-	
-	foreach ($tweets as $tweet) {
-		$title = remove_urls($tweet->text);
-		$description = parse_urls($tweet->text, $tweet->id);
-		$link = $tweetURL . $tweet->id;
-		$pubDate = $tweet->created_at;
-		$author = $user_o->data->username;
-		$mediaContent = "";
-		if (isset($includes) && isset($tweet->attachments) && isset($tweet->attachments->media_keys)) {
-			foreach ($tweet->attachments->media_keys as $media_key) {
-				foreach ($includes->media as $media) {
-					if ($media_key == $media->media_key) {
-						if (isset($media->preview_image_url)) {
-							$mediaContent = make_rss_media($media->preview_image_url, $mediaContent);
-						}
-						if (isset($media->url)) {
-							$mediaContent = make_rss_media($media->url, $mediaContent);
-						}
-
-					}
+//Figure out if they're allowed to make this query
+$allowed = true;
+if (isset($config['access_control'])) {
+	$allowed = false;
+	$access = $config['access_control'];
+	if (isset($access[$clientid])) {
+		if ($access[$clientid] == "*") {
+			$allowed = true;
+		} else {
+			if (is_array($access[$clientid])) {
+				if (in_array($twitterid, $access[$clientid])) {
+					$allowed = true;
 				}
 			}
 		}
-		output_rss_post($author, $title, $link, $description, $mediaContent, $pubDate);
 	}
-	output_rss_footer();
-} else {
-	die ("No valid Twitter ID specified. Valid Twitter IDs are numeric, and may be encoded. You can get the numeric ID from a username with sites like https://tweeterid.com/");
+}
+if (!$allowed) {
+	die ("Specified user does not have access to requested Twitter ID.");
 }
 
+echo "getting tweets from " . $twitterid . " for user " . $clientid . " because allowed =" . $allowed;
+die();
+
+//Actually get twitter feed and return as RSS XML
+//TODO: cache
+$userdata = get_twitter_userdata($twitterid, $bearer_token);
+$user_o = json_decode($userdata);
+
+$tweetURL = "https://twitter.com/" . $user_o->data->username;
+output_rss_header($user_o->data->name . " Tweets", $tweetURL, "@" . $user_o->data->username, $user_o->data->profile_image_url);
+$tweetURL = $tweetURL . "/status/";
+
+$tweetdata = get_twitter_tweetsforuser($twitterid, $bearer_token);
+$tweets_o = json_decode($tweetdata);
+$tweets = $tweets_o->data;
+if (isset($tweets_o->includes)) {
+	$includes = $tweets_o->includes;
+}
+
+foreach ($tweets as $tweet) {
+	$title = remove_urls($tweet->text);
+	$description = parse_urls($tweet->text, $tweet->id);
+	$link = $tweetURL . $tweet->id;
+	$pubDate = $tweet->created_at;
+	$author = $user_o->data->username;
+	$mediaContent = "";
+	if (isset($includes) && isset($tweet->attachments) && isset($tweet->attachments->media_keys)) {
+		foreach ($tweet->attachments->media_keys as $media_key) {
+			foreach ($includes->media as $media) {
+				if ($media_key == $media->media_key) {
+					if (isset($media->preview_image_url)) {
+						$mediaContent = make_rss_media($media->preview_image_url, $mediaContent);
+					}
+					if (isset($media->url)) {
+						$mediaContent = make_rss_media($media->url, $mediaContent);
+					}
+
+				}
+			}
+		}
+	}
+	output_rss_post($author, $title, $link, $description, $mediaContent, $pubDate);
+}
+output_rss_footer();
+
+
 //Twitter API v2 calls
-function get_twitter_userdata($uid, $bearer_token) {
-	$url = "https://api.twitter.com/2/users/" . $uid . "?user.fields=profile_image_url";
+function get_twitter_userdata($twitterid, $bearer_token) {
+	$url = "https://api.twitter.com/2/users/" . $twitterid . "?user.fields=profile_image_url";
 	$ch = init_curl_fortwitter($url, $bearer_token);
 
 	$response = curl_exec($ch);
@@ -71,8 +110,8 @@ function get_twitter_userdata($uid, $bearer_token) {
 	return $response;
 }
 
-function get_twitter_tweetsforuser($uid, $bearer_token) {
-	$url = "https://api.twitter.com/2/users/" . $uid . "/tweets?max_results=20&tweet.fields=attachments,created_at&exclude=replies,retweets&expansions=attachments.media_keys&media.fields=type,url,preview_image_url,alt_text";
+function get_twitter_tweetsforuser($twitterid, $bearer_token) {
+	$url = "https://api.twitter.com/2/users/" . $twitterid . "/tweets?max_results=20&tweet.fields=attachments,created_at&exclude=replies,retweets&expansions=attachments.media_keys&media.fields=type,url,preview_image_url,alt_text";
 	$ch = init_curl_fortwitter($url, $bearer_token);
 
 	$response = curl_exec($ch);
